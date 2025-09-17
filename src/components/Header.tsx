bc9ddd2cfdd4acbd009ba4a687f8b7c0eb68fc90
Hakkotako <davidextol@gmail.com>
2025-09-17 12:09:23 +0200

    Header logo made home btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 "use client";
-
+import { Box, Typography } from "@mui/material";
+import NextLink from "next/link";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
 import Image from "next/image";
 import navData from "@/data/navigation.json"; // importér direkte
 
@@ -20,13 +19,17 @@ export default function Header({ navTitle, logoSrc, navBgColor }: HeaderProps) {
     <AppBar position="fixed" sx={{ backgroundColor: navBgColor || "rgba(14, 13, 13, 0.41)", backdropFilter: "blur(1px)" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between", padding: "25px" , maxHeight: 100, minHeight: 50, ml: 10, mr: 10}}>
         {logoSrc && (
-          <Image
-            src={logoSrc}
-            alt="logo"
-            width={60}
-            height={60}
-            style={{ marginRight: "10px", objectFit: "contain"}}
-          />
+          <NextLink href="/" passHref>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Image
+                src={logoSrc}
+                alt="logo"
+                width={60}
+                height={60}
+                style={{ marginRight: "10px", objectFit: "contain" }}
+              />
+            </Box>
+          </NextLink>
         )}
         <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: 'bold', fontSize: '24px' }}>
           {navTitle || "Welcome"}
